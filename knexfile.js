4584e13bf05bd727f108df13b07225fc59adcb44
Guilherme Santos <guicarsiqsantos@gmail.com>
2021-01-10 15:10:56 -0300

    alteração knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -18,13 +18,11 @@ module.exports = {
   production: {
     client: "postgres",
     url: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10,
-    },
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "migrations"),
-      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: path.resolve(__dirname, "src", "database", "seeds"),
     },
   },
 
