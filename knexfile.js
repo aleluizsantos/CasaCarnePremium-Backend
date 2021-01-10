7dbc8563fe1e3d7260c3e2760d4f84d2d39b1fd6
Guilherme Santos <guicarsiqsantos@gmail.com>
2021-01-10 14:40:58 -0300

    alterado Knexfile

--- knexfile.js
+++ knexfile.js
@@ -18,16 +18,13 @@ module.exports = {
   production: {
     client: "postgres",
     url: process.env.DATABASE_URL,
-    connection: {
-      filename: path.resolve(__dirname, "src", "database", "test.sqlite"),
+    pool: {
+      min: 2,
+      max: 10,
     },
     migrations: {
-      directory: path.resolve(__dirname, "src", "database", "migrations"),
+      tableName: "knex_migrations",
     },
-    seeds: {
-      directory: path.resolve(__dirname, "src", "database", "seeds"),
-    },
-    useNullAsDefault: true,
   },
 
   staging: {
