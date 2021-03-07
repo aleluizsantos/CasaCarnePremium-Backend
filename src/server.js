a88d3dab14b21348ec461134a799445985ca64a0
Guilherme Santos <guicarsiqsantos@gmail.com>
2021-03-07 12:54:12 -0300

    upgrade app.use(express.json)

--- src/server.js
+++ src/server.js
@@ -33,8 +33,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 //Referenciando os CONTROLLER repassando a app
